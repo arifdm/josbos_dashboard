3c7292cb491641d058fb03dc4c9c545dd8aaa9c8
Arif Dian M <arifdm@gmail.com>
2023-10-30 16:37:42 +0700

    Update

--- components/About.jsx
+++ components/About.jsx
@@ -46,7 +46,8 @@ export default function Example() {
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Dengan aplikasi yang dirancang dengan sangat baik untuk memudahkan
             Pengguna saat melakukan pesanan dengan 4 pilihan cara pesan sehingga
-            mendapatkan layanan yang berkualitas dengan tarif yang pas.
+            mendapatkan layanan yang berkualitas dengan tarif yang hemat dan
+            pas.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
