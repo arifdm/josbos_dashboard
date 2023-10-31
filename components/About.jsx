4342158cfb3af838cf571827d4ea20f0e39a9fb4
Arif Dian M <arifdm@gmail.com>
2023-10-31 08:10:50 +0700

    Update wording

--- components/About.jsx
+++ components/About.jsx
@@ -34,7 +34,7 @@ const features = [
 
 export default function Example() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div id="about" className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-yellow-500">
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -37,16 +37,10 @@ export default function Example() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a
-                href="#"
+                href="#about"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white"
-              >
-                Learn more <span aria-hidden="true">→</span>
+                Get Information
               </a>
             </div>
           </div>
