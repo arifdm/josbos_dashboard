70a012f91bcf6716b89353af464fa3ef1c0128e7
Arif Dian M <arifdm@gmail.com>
2024-01-19 12:59:25 +0700

    Update Bank Account

--- app/api/bankAccount/[id]/route.jsx
+++ app/api/bankAccount/[id]/route.jsx
@@ -1,16 +1,8 @@
 import prisma from "@/prisma/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
 export async function GET(request, { params }) {
-  // const accessToken = request.headers.get("Authorization");
-
-  // if (!accessToken) {
-  //   return NextResponse.json({
-  //     status: false,
-  //     error: "Authentication token required!",
-  //   });
-  // }
-
   const data = await prisma.bankAccount.findFirst({
     where: { id: params?.id },
   });
@@ -24,21 +16,58 @@ export async function GET(request, { params }) {
 }
 
 export async function PUT(request, { params }) {
-  const { title, content, image } = await request.json();
-  try {
-    const data = await prisma.bankAccount.update({
-      where: { id: params?.id },
-      data: { title, content, image },
-    });
-    return NextResponse.json({
-      status: true,
-      message: "Update successfully",
-      data,
-    });
-  } catch (error) {
+  const accessToken = request.headers.get("Authorization");
+
+  const searchParams = request.nextUrl.searchParams;
+  const user = searchParams.get("user");
+
+  const { brandName, accountName, number } = await request.json();
+  console.log("ID: ", params?.id);
+  console.log("BODY: ", brandName, accountName, number);
+
+  if (!accessToken) {
     return NextResponse.json({
       status: false,
-      error: "Update failed",
+      error: "Silakan masukkan token...!",
     });
+  } else {
+    let decoded;
+    try {
+      const bearer = accessToken.replace("Bearer ", "");
+      decoded = await jwt.verify(bearer, process.env.JWT_SECRET);
+    } catch (error) {
+      return NextResponse.json({
+        status: false,
+        error: "Access Token tidak valid...!",
+      });
+    }
+
+    if (decoded.role !== user) {
+      return NextResponse.json({
+        status: false,
+        error: "Anda tidak memiliki akses...!",
+      });
+    }
+
+    try {
+      const data = await prisma.bankAccount.update({
+        where: { id: params?.id },
+        data: {
+          brandName,
+          accountName,
+          number,
+        },
+      });
+      return NextResponse.json({
+        status: true,
+        message: "Update successfully",
+        data,
+      });
+    } catch (error) {
+      return NextResponse.json({
+        status: false,
+        error: "Update failed",
+      });
+    }
   }
 }
