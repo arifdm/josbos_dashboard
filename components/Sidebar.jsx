e26d1d451901d59104272f5479cbd14d60e6be3e
Arif Dian M <arifdm@gmail.com>
2024-03-26 10:32:08 +0700

    Update menu

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -19,27 +19,28 @@ import {
 
 export default function Menu() {
   const pathname = usePathname();
+  console.log("PATHNAME: ", pathname);
 
   return (
     <div className="w-1/6 bg-slate-100 hidden lg:block px-4 py-8 text-sm max-h-full min-h-screen">
-      <div
-        className={`font-bold mt-2 mx-1 hover:text-primary ${
-          pathname === "/mainboard"
-            ? "text-primary"
-            : "transition-color duration-400 delay-100 ease-in-out"
-        }`}
-      >
-        <Link href="/mainboard">Dashboard</Link>
-      </div>
+      <Link href="/mainboard">
+        <div
+          className={`font-bold mt-2 mx-1 hover:text-primary ${
+            pathname === "/mainboard" && "text-primary"
+          }`}
+        >
+          Dashboard
+        </div>
+      </Link>
       <div className="font-bold mt-6 mx-1">Pesanan</div>
       <ul className="mt-2 leading-8">
         <li>
           <Link href="/pesananMasuk">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
                 pathname === "/pesananMasuk"
                   ? "text-amber-500"
-                  : "transition-color duration-400 delay-100 ease-in-out"
+                  : "text-slate-500"
               }`}
             >
               <MdAssignmentReturned className="w-5 h-5 inline-block mr-2" />
@@ -50,10 +51,10 @@ export default function Menu() {
         <li>
           <Link href="/pesananDibatalkan">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
                 pathname === "/pesananDibatalkan"
                   ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+                  : "text-slate-500"
               }`}
             >
               <MdAssignmentLate className="w-5 h-5 inline-block mr-2" />
@@ -64,10 +65,10 @@ export default function Menu() {
         <li>
           <Link href="/pesananSelesai">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
                 pathname === "/pesananSelesai"
                   ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+                  : "text-slate-500"
               }`}
             >
               <MdAssignmentTurnedIn className="w-5 h-5 inline-block mr-2" />
@@ -81,10 +82,8 @@ export default function Menu() {
         <li>
           <Link href="/users">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/users"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/users" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <UsersIcon className="w-5 h-5 inline-block mr-2" />
@@ -95,10 +94,8 @@ export default function Menu() {
         <li>
           <Link href="/mitra">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/mitra"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/mitra" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <UserGroupIcon className="w-5 h-5 inline-block mr-2" />
@@ -112,10 +109,8 @@ export default function Menu() {
         <li>
           <Link href="/articles">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/articles"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/articles" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <NewspaperIcon className="w-5 h-5 inline-block mr-2" />
@@ -126,10 +121,8 @@ export default function Menu() {
         <li>
           <Link href="/promos">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/promos"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/promos" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <BookmarkIcon className="w-5 h-5 inline-block mr-2" />
@@ -143,10 +136,8 @@ export default function Menu() {
         <li>
           <Link href="/cities">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/cities"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/cities" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <BuildingOffice2Icon className="w-5 h-5 inline-block mr-2" />
@@ -157,10 +148,8 @@ export default function Menu() {
         <li>
           <Link href="/vehicles">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/vehicles"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/vehicles" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <TruckIcon className="w-5 h-5 inline-block mr-2" />
@@ -171,10 +160,8 @@ export default function Menu() {
         <li>
           <Link href="/services">
             <div
-              className={`text-slate-500 px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
-                pathname === "/services"
-                  ? "text-amber-500"
-                  : "transition-all duration-400 delay-100 ease-in-out"
+              className={`px-2 rounded-sm cursor-pointer hover:text-amber-500 ${
+                pathname === "/services" ? "text-amber-500" : "text-slate-500"
               }`}
             >
               <RectangleGroupIcon className="w-5 h-5 inline-block mr-2" />
